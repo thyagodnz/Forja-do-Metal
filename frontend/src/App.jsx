621588905fcb56e8c9434ed4cb25e658619da213
Thyago Diniz <thyago.odiniz007@gmail.com>
2026-02-14 17:58:14 -0300

    Revert "feat: inicio da tela de cadastro"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home.jsx'
-import Header from './components/Header/Header.jsx'
-import Register from './pages/Register/Register.jsx'
 
 function App() {
 
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
